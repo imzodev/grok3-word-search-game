a6e26b06ee2971de6300170303ff9d68d81363ae
Irving Zamora <imzodev@gmail.com>
2025-02-20 15:15:57 -0700

    feat: Add Vercel Analytics for improved user tracking

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+import { Analytics } from "@vercel/analytics/react"
+
 root.render(
   <React.StrictMode>
+    <Analytics />
     <App />
   </React.StrictMode>
 );
